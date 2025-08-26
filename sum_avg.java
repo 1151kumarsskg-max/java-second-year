5d5d67a8ff3c72e294820e9d017487a19178500c
1151kumarsskg-max <1151kumarsskg@gmail.com>
2025-08-26 15:30:53 +0530

    Add files via upload

---
+++ sum_avg.java
@@ -0,0 +1,12 @@
+public class sum_avg {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        double average = (double) sum / numbers.length;
+        System.out.println("Sum: " + sum);
+        System.out.println("Average: " + average);
+    }
+}
