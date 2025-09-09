26342d8984c59a457d779877fe8472666260e5c3
1151kumarsskg-max <1151kumarsskg@gmail.com>
2025-09-09 15:17:58 +0530

    Add files via upload

---
+++ RightShiftExample.java
@@ -0,0 +1,19 @@
+// public class LeftShiftExample {
+//     public static void main(String[] args) {
+//         int a = 5; // 0101
+
+//         int result = a << 2; // 5 << 2 = 20
+
+//         System.out.println("a << 2 = " + result);
+//     }
+// }
+ 
+public class RightShiftExample {
+    public static void main(String[] args) {
+        int a = 20; // 10100
+
+        int result = a >> 2; // 20 >> 2 = 5
+
+        System.out.println("a >> 2 = " + result);
+    }
+}
