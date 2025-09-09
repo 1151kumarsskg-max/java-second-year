e5037433c1e371b4e69d34da4dbab32538d0aeef
1151kumarsskg-max <1151kumarsskg@gmail.com>
2025-09-09 15:26:43 +0530

    Add files via upload

---
+++ NotEqualExample.java
@@ -0,0 +1,29 @@
+// public class EqualExample {
+//     public static void main(String[] args) {
+//         int a = 5;
+//         int b = 10;
+
+//         if (a == b) {
+//             System.out.println("a is equal to b");
+//         } else {
+//             System.out.println("a is not equal to b");
+//         }
+//     }
+// }
+
+// public class EqualToExample {
+//     public static void main(String[] args) {
+//         int x = 10, y = 10;
+
+//         System.out.println("x == y: " + (x == y)); // true
+//     }
+// }
+
+public class NotEqualExample {
+    public static void main(String[] args) {
+        int x = 10, y = 20;
+
+        System.out.println("x != y: " + (x != y)); // true
+    }
+}
+
