637f5e8c4186422b06dbcd50c0fec8c58e573363
1151kumarsskg-max <1151kumarsskg@gmail.com>
2025-09-15 12:30:41 +0530

    Add files via upload

---
+++ conditional.java
@@ -0,0 +1,7 @@
+public class conditional {
+    public static void main(String[] args) {
+        int number = 10;
+        String result = (number % 2 == 0) ? "even" : "odd";
+        System.out.println("the number is: " + result);
+    }
+}
